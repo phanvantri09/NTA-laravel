fd7b3fd6f9ffed795da73e7eadb6aa7307a5751d
phanvantri09 <hulk09022000@gmail.com>
2021-09-26 01:10:54 +0700

    update layout home

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('admin')->group(function () {
+    // route User
     Route::get('/listuser','AdminController@listUser')->name('admin.listUser');
     Route::get('/edituser','AdminController@editUser')->name('admin.edituser');
+    // route BOOK
     Route::get('/listbook','BookController@list')->name('admin.listbook');
     Route::get('/editbook/{id}','BookController@edit')->name('admin.editbook');
     Route::PUT('/updatebook/{id}','BookController@update')->name('admin.updatebook');
     Route::get('/addbook','BookController@add')->name('admin.addbook');
     Route::POST('/addbook','BookController@create')->name('admin.add');
     Route::DELETE('/deletebook/{id}','BookController@delete')->name('admin.deletebook');
+
+});
+Route::prefix('bookshop')->group(function () {
+    Route::get('/home','HomeController@index')->name('bookshop.home');
+    
 });
